0686465e100d48df3a453138a91202770c6e271f
Helper2020 <12247834+Helper2020@users.noreply.github.com>
2021-04-25 03:23:16 -0400

    Starter Code

---
+++ budget.py
@@ -0,0 +1,16 @@
+class Budget:
+    def __init__(self, category, amount):
+        self.category = category
+        self.amount = amount
+
+    def deposit(self):
+        pass
+
+    def check_balance(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def transfer(self):
+        pass
